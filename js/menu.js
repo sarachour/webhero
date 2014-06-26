7a42e78b20df4f675947258a785506fb008810a0
sachour <sachour@mit.edu>
2014-06-25 23:43:59 -0400

    added placeholder files

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+
+function SetupLayout(){
+	$('.menu').layout({resize: false})
+	$('#play-game').click(function(d){
+		
+	});
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  SetupLayout();
+})
