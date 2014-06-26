14e062866171ed1dcd9cc3ab595310214a9b9790
sachour <sachour@mit.edu>
2014-06-26 00:46:04 -0400

    tet

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,13 @@
 function SetupLayout(){
 	$('.menu').layout({resize: false})
 	$('#play-game').click(function(d){
-		
+		chrome.tabs.query({currentWindow: true, active:true}, function(tabs){
+			if(tabs.length == 1){
+				chrome.tabs.executeScript(tabs[0].id, {file: "js/web.js"}, function(){
+					console.log("injected");
+				});
+			}
+		});
 	});
 
 }
---
+++ js/web.js
@@ -0,0 +1,27 @@
+console.log(document);
+/*
+Color Scheme: Css File
+   - color frequencies
+
+Map Layout: 
+   - randomly generated from seed OR cross compiled from document object.
+       - some sort of smart traversal through walls.
+
+Tileset:
+   - Color Palette: pulled from bg colors of html entities.
+
+Spells:
+   - on keyboard, must spell most common unique words on page. Harder words -> stronger spells
+   - spell names are interesting word names. 
+
+Monsters:
+   - color: generated from images on the screen. (image color palette -> monster color palette)
+   - parts: mix & match using word as a seed.
+   - stats: 
+
+Items:
+   - [n primitive types]: color, size variation from font color-size
+
+Doors:
+   - Links in page (tab load -> compile -> new level).
+*/
